910df34e63aa6927bea605cc4e09f458789e7378
Joar <joar1.kodehode@gmail.com>
2022-06-28 13:36:32 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
     <Header/>
-    <Navbar/>
+    {/* <Navbar/> */}
     <h1>TEST</h1>
     <img src={logo} className="App-logo" alt="logo" width="80" />
     </>
