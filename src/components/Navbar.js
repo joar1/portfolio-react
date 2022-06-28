14e11db70dc3ed79e14149fef993db1871c6496a
Joar <joar1.kodehode@gmail.com>
2022-06-28 13:41:52 +0200

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
 const Navbar = () =>{
    return (
-      <Navbar>
-         <ul>
-            <li>Hjem</li>
-            <li>Om Meg</li>
-            <li>Prosjekter</li>
-            <li>LinkedIn</li>
-         </ul>
-      </Navbar>
+      <ul>
+         <li>Hjem</li>
+         <li>Om Meg</li>
+         <li>Prosjekter</li>
+         <li>LinkedIn</li>
+      </ul>
    );
 };
 
