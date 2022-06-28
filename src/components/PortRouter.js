b3f7a3aaec01958fe57c7aa51026005c05ab996b
Joar <joar1.kodehode@gmail.com>
2022-06-28 14:56:36 +0200

    .

--- src/components/PortRouter.js
+++ src/components/PortRouter.js
@@ -1,12 +1,18 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Hjem from "../pages/Hjem";
-import OmMeg from "../pages/Ommeg";
+import OmMeg from "../pages/OmMeg";
 import Prosjekter from "../pages/Prosjekter";
 
 const PortRouter = () => {
    return(
-      <div>PortRouter</div>
+      <HashRouter>
+         <Routes>
+            <Route path="Hjem" element={<Hjem/>}/>
+            <Route path="OmMeg" element={<OmMeg/>}/>
+            <Route path="Prosjekter" element={<Prosjekter/>}/>
+         </Routes>
+      </HashRouter>
    )
 }
 
