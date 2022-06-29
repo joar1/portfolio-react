de9150dcd6ec975c219bfaf462b8c09bab7f2bc4
Joar <joar1.kodehode@gmail.com>
2022-06-29 13:19:32 +0200

    style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,32 +2,54 @@ import styled from "styled-components";
 
 const Navbar = () =>{
    return (
-      <PimpUl>
-         <PimpLi className="nav-item" >Hjem</PimpLi>
-         <PimpLi className="nav-item">Om Meg</PimpLi>
-         <PimpLi className="nav-item">Prosjekter</PimpLi>
-         <PimpLi className="nav-item">LinkedIn</PimpLi>
-      </PimpUl>
+      <>
+      <PimpNav>
+         <PimpA href="#"><BigA>Hjem</BigA></PimpA>
+         <PimpUl>
+            <PimpLi className="nav-item"><PimpA href="#">Om Meg</PimpA></PimpLi>
+            <PimpLi className="nav-item"><PimpA href="#">Prosjekter</PimpA></PimpLi>
+            <PimpLi className="nav-item"><PimpA href="#">LinkedIn</PimpA></PimpLi>
+         </PimpUl>
+      </PimpNav>
+      </>
    );
 };
 
 export default Navbar;
 
 
+const PimpNav = styled.nav`
+   margin: 0;
+   padding: 0;
+   background:#1C1C1C;
+   display: flex;
+   justify-content: space-between;
+`
+
 const PimpUl = styled.ul`
    margin: 0;
    padding: 0;
-   overflow:hidden;
-   background:#1C1C1C;
 `
 
 const PimpLi = styled.li`
    margin: 0;
-   padding: 10px;
+   padding: 10px 12px 10px 0px;
    display: block;
    float: left;
-   text-decoration: none;
    font-size: 20px;
    font-weight: bolder;
    cursor: pointer;
+`
+const BigA = styled.a`
+   font-size: 2rem;
+   padding-left:15px;
+`
+
+const PimpA = styled.a`
+   align-items: center;
+   color: blanchedalmond;
+   text-decoration: none;
+   &:hover{
+      color:gold;
+   }
 `
